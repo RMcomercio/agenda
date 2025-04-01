d56b3fa0b21ca14853c39093b40d37fe9fc5f878
Reginaldo Campos <regicid@gmail.com>
2025-03-31 22:32:34 -0300

    basico da agenda pronto

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 # id (primary key - automático)
@@ -10,6 +11,15 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+
+    name = models.CharField(max_length=50)
+  
+    def __str__(self):
+            return f'{self.name} '
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -19,6 +29,18 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default =True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        verbose_name=("categorias"),
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+       
 
     def __str__(self):
         return f'{self.first_name} { self.last_name}'
--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('contact.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
